3f97fe28d0dcb4c467338263eff9253dd6998a08
wavaronr <alexney1@hotmail.com>
2023-01-11 21:08:01 -0500

    jbTema3Ejercicio

---
+++ jbTema3Ejercicio/src/main/java/com/jbtema3ejercicio/jbtema3ejercicio/JbTema3Ejercicio.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.jbtema3ejercicio.jbtema3ejercicio;
+
+/**
+ *
+ * @author Wilmer Alexander Varon Rodriguez
+ */
+public class JbTema3Ejercicio {
+
+    public static void main(String[] args) {
+        String[] concatenar={"Wilmer","Alexander","Varon","Rodriguez"};
+        String concatenado="";
+        for(String unir : concatenar){
+            
+        concatenado+=unir+" ";
+        }
+        System.out.println(concatenado);
+    }
+}
